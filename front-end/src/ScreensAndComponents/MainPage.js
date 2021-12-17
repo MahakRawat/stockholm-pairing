b02d6f431eb3c034d5a3bb21a3b2dc66d78ccbcb
Mahak Rawat <mahakrawat19@gmail.com>
2021-12-17 13:19:45 +0530

    some changes in main page

--- front-end/src/ScreensAndComponents/MainPage.js
+++ front-end/src/ScreensAndComponents/MainPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ProfileTemp from './ProfileTemp.js';
 import User from "@material-ui/icons/Person";
 import Ficon from "@material-ui/icons/FilterVintageTwoTone";
-import Chat from "@material-ui/icons/ChatBubble";
+import Chat from "@material-ui/icons/Chat";
 import Axios from 'axios';
 import {useState,useEffect} from 'react';
 
@@ -11,17 +11,18 @@ function MainPage() {
    const [list,setlist]=useState([]);
     
    useEffect(() => {
-     Axios.get('http://127.0.0.1:4000/search?gender=male&preferred_gender=female')
+     Axios.get('http://127.0.0.1:4000/search?gender=male&preferred_gender=female')//manually done
      .then(res=>{setloading(false); setlist(res.data);})
      .catch(e=>console.log(e));   
    },[])
     return (
         <div>
         <div className="nav">
-         <div><User style={{fontSize:'3rem'}}></User></div>
-         <div><Ficon style={{fontSize:'3rem'}}></Ficon></div>
-         <div><Chat style={{fontSize:'3rem'}}></Chat></div>
+         <div><User style={{color:'#0066b2',fontSize:'3rem'}}></User></div>
+         <div><Ficon style={{color:'#0066b2',fontSize:"3.2rem"}} /></div>
+         <div><Chat style={{color:'#0066b2',fontSize:'3rem'}}></Chat></div>
         </div>
+        <br />
         <div className="container">
         <div className="filters">
         </div>
@@ -30,7 +31,7 @@ function MainPage() {
              loading?<div>loading</div>
               :
                 list.map(x=>
-                    <ProfileTemp name={x.user_name} images={x.images} address={x.address} about={x.about}></ProfileTemp>
+                    <ProfileTemp name={x.user_name} images={x.images} address={x.address} about={x.about} dob={x.dob} interests={x.interests}></ProfileTemp>
                 )  
           }
         </div>
--- front-end/src/ScreensAndComponents/ProfileTemp.js
+++ front-end/src/ScreensAndComponents/ProfileTemp.js
@@ -10,23 +10,30 @@ function ProfileTemp(props) {
     console.log(name + ' left the screen!')
   }
   return(
-   <div>
+   <div style={{padding:'0rem 0rem 0rem 7rem'}}>
        <TinderCard className='swipe' key={props.name} onSwipe={(dir) => swiped(dir, props.name)} onCardLeftScreen={() => outOfFrame(props.name)}>
             
            {
-            
                            <div style={{ backgroundImage: 'url(' + props.images[0] + ')' , 
                           backgroundSize:'cover',
-                          backgroundPosition:'center',height:'50vh',
-                          width:'26vw'
+                          backgroundPosition:'center',
+                          height:'50vh',
+                          width:'26vw',
                           }} 
-                          className='card'></div>
-                         
-        
+                          className='card'>
+                              <div style={{padding:'22rem 2rem 0rem 1rem'}}>
+                              
+                              </div>
+                          </div>
+            
             }
-              <h3>{props.name}</h3>
-              <h3>{props.address}</h3>
-              <p>{props.about}</p>
+            <div style={{backgroundColor:'#0066b2', width:'26vw',height:'13rem',padding:'0.5rem 0rem 1rem 0rem'}}>
+                <h4 style={{backgroundColor:'whitesmoke'}}>{props.name}</h4>
+                <h4 style={{backgroundColor:'whitesmoke'}}>{props.address}</h4>
+                <h4 style={{backgroundColor:'whitesmoke'}}>{props.dob}</h4>
+                <h4 style={{backgroundColor:'whitesmoke'}}>{props.interests}</h4>
+                <h4 style={{backgroundColor:'whitesmoke'}}>{props.about}</h4>
+            </div>
       </TinderCard>
    </div>
   );
